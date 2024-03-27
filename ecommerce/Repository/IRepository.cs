03966a4201e269692c8fee5cc73b617c4a0ad342
sadek <mahmoudsadek.1905@gmail.com>
2024-03-27 19:47:09 +0200

    added the IRepository<T> intrface

---
+++ ecommerce/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ecommerce.Repository
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Insert(T item);
+        void Update(T item);
+        void Delete(T item);
+        void Save();
+    }
+}
