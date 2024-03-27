c59d694817b2a3a71354496c6c105875323c0207
sadek <mahmoudsadek.1905@gmail.com>
2024-03-27 21:07:53 +0200

    Added Product Service

---
+++ ecommerce/Services/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using ecommerce.Repository;
+
+namespace ecommerce.Services
+{
+    public class ProductService
+    {
+        private readonly IProductRepository repository;
+        public ProductService(IProductRepository _repository)
+        {
+            repository = _repository;
+        }
+
+    }
+}
