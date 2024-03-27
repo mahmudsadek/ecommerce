441f9fcbc0a2f84b71df3509001f220341f98c77
sadek <mahmoudsadek.1905@gmail.com>
2024-03-27 21:02:39 +0200

    added product repo

---
+++ ecommerce/Repository/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using ecommerce.Models;
+namespace ecommerce.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(Context _context) : base(_context)
+        {
+        }
+
+    }
+}
