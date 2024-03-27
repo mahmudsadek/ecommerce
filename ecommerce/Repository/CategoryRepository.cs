5c27aa1c8dac0257e7d20e7273255bf79d4193b9
sadek <mahmoudsadek.1905@gmail.com>
2024-03-27 21:03:06 +0200

    added category repo and Icategory repo interface

---
+++ ecommerce/Repository/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ecommerce.Models;
+
+namespace ecommerce.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(Context _context) : base(_context)
+        {
+        }
+
+    }
+}
---
+++ ecommerce/Repository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using ecommerce.Models;
+
+namespace ecommerce.Repository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+
+    }
+}
