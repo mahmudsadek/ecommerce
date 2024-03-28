58394853ea4526850ec6e77fbf081f92cb5d821b
reefaey <abdallah.hussien101@gmail.com>
2024-03-29 01:12:37 +0200

    Refaey - Add CategoryRepo, ICategory, and CategoryController

--- ecommerce/Repository/CategoryRepository.cs
+++ ecommerce/Repository/CategoryRepository.cs
@@ -1,11 +1,103 @@
 ﻿using ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ecommerce.Repository
 {
     public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        Context Context;
         public CategoryRepository(Context _context) : base(_context)
         {
+            Context = _context;
+        }
+
+        public List<Category> GetAll(string include = null)
+        {
+            if (include != null)
+            {
+                return Context.Category.Include(include).ToList();
+            }
+
+            return Context.Category.ToList();
+        }
+
+        public List<Product> GetAllProductsInCategory(int CategoryId)
+        {
+            Category category = Get(CategoryId);
+            return category.Products;
+        }
+
+        public Category Get(int id)
+        {
+            return Context.Category.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Category GetCategoryByName(string name)
+        {
+            return Context.Category.FirstOrDefault(c => c.Name == name);
+        }
+
+        List<Category> Get(Func<Category, bool> where)
+        {
+            return Context.Category.Where(where).ToList();
+        }
+
+        public void Insert(Category item)
+        {
+            Context.Add(item);
+        }
+
+        //void ChangeProductCategory(int product_id, int old_category_id, int new_category_id)
+        //{
+        //    Product product = Get(product_id);
+        //}
+
+        public void TransferAllProductsToAnotherCategory(int OldCategoryId, int NewCategoryId) // The id of the new category
+        {
+
+            List<Product> products = Get(OldCategoryId).Products;
+
+            foreach (Product product in products)
+            {
+                product.CategoryId = NewCategoryId;
+            }
+        }
+
+        public void RenameCategory(string categoryName, string NewName)
+        {
+            GetCategoryByName(categoryName).Name = NewName;
+        }
+
+        public void DeleteAllProductsInCategory(int CategoryId)
+        { 
+            Category Category = Get(CategoryId);
+            List<Product> products = Category.Products.ToList();
+
+            foreach (Product product in products)
+            {
+                Context.Remove(product);
+            }
+        }
+
+        public void Update(Category item)
+        {
+            Context.Update(item);
+        }
+
+        public void Delete(int id)
+        {
+            Category item = Get(id);
+
+            if(item.Products.Count == 0) // To Check if the category is empty before delete it
+            {
+                Context.Remove(item);
+            }
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
         }
 
     }
