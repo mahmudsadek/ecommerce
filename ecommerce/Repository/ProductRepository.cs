d6379444dbca851cdae973127ae505259b22be26
Mahmoud-Mohamed-Maher <mahmoud.do7aa@gmail.com>
2024-03-28 07:34:20 +0200

    add methods in  IProductRepository and ProductRepository

--- ecommerce/Repository/ProductRepository.cs
+++ ecommerce/Repository/ProductRepository.cs
@@ -1,11 +1,53 @@
 ﻿using ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
 namespace ecommerce.Repository
 {
     public class ProductRepository : Repository<Product>, IProductRepository
     {
+
         public ProductRepository(Context _context) : base(_context)
         {
         }
+
+        public void Insert(Product product)
+        {
+            Context.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            Context.Update(product);
+        }
+
+        public List<Product> GetAll(string include = null)
+        {
+            if (include == null)
+            {
+                return Context.Product.ToList();
+            }
+            return Context.Product.Include(include).ToList();
+        }
+
+        public Product Get(int Id)
+        {
+            return Context.Product.FirstOrDefault(p=>p.Id==Id);
+        }
+        public List<Product> Get(Func<Product, bool> where)
+        {
+            return Context.Product.Where(where).ToList();
+        }
+
+        public void Delete(Product product)
+        {
+            Context.Remove(product);
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+       
+
 
     }
 }
