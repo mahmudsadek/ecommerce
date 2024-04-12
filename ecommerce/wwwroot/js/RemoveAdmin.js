8e85827736c19cd280f3817921be88966da22cbc
saeed mohamed <saeedm1113@gmail.com>
2024-04-12 16:40:52 +0200

    saeed : dashboard >> change role &  delete user

--- ecommerce/wwwroot/js/RemoveAdmin.js
+++
@@ -1,4 +0,0 @@
-﻿function RemoveAdmin(name) {
-    document.getElementById("text").innerText = `That will make ${name} unauthorized to access any data..\n  Are you sure to remove ${name} from admin?`;
-    document.getElementById("addBtn").href = `/account/confirmRemoveAdmin?userName=${name}`;
-}
