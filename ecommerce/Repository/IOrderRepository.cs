856324a65b9c6a6edead5768d6aeae5b643dded9
sadek <mahmoudsadek.1905@gmail.com>
2024-04-14 22:26:50 +0200

    edit shipment and order repos

--- ecommerce/Repository/IOrderRepository.cs
+++ ecommerce/Repository/IOrderRepository.cs
@@ -5,5 +5,6 @@ namespace ecommerce.Repository
     public interface IOrderRepository : IRepository<Order>
     {
         // Omar : any logic specified to Order Only more than CRUD
+        Order InsertOrder(Order order);
     }
 }
